0b56e016125e1544ba23e7754f00b9ace656b95d
valentinesamuel <valentinesamuel2580@gmail.com>
2022-08-25 15:04:29 +0000

    added 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 import sys
+import calculator_1 as calc
 if __name__ == '__main__':
     argv_len = len(sys.argv) - 1
+    ops = {'+', '+', '-', '/'}
     if (not argv_len == 3):
-        print('Usage: ', end='')
-        for idx in range(0, len(sys.argv)):
-            print('{}'.format(sys.argv[idx]), end=" ")
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operator = sys.argv[2]
+    if not sys.argv[2] in ops:
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+    if operator == '+':
+        result = calc.add(a, b)
+        print('{} {} {} = {}'.format(a, operator, b, result))
+    elif operator == '-':
+        result = calc.sub(a, b)
+        print('{} {} {} = {}'.format(a, operator, b, result))
+    elif operator == '*':
+        result = calc.mul(a, b)
+        print('{} {} {} = {}'.format(a, operator, b, result))
+    elif operator == '/':
+        result = calc.div(a, b)
+        print('{} {} {} = {}'.format(a, operator, b, result))
+    print('')
