3a4ec3c25aca8fa4880ae3bfceb1453c709f2b33
valentinesamuel <valentinesamuel2580@gmail.com>
2022-08-25 12:27:03 +0000

    correct 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
         print('{:d} arguments.'.format(argv_len))
     elif (argv_len == 1):
         print('{:d} argument:'.format(argv_len))
-        print('{:d}: {:s}'.format(argv_len ,sys.argv[1]))
+        print('{:d}: {:s}'.format(argv_len, sys.argv[1]))
     elif (argv_len > 1):
         print('{:d} arguments:'.format(argv_len))
         for idx in range(1, argv_len + 1):
