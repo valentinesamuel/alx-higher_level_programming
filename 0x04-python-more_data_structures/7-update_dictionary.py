64a8ce20eee90652051a4b6c2cc9188c026bae22
valentinesamuel <valentinesamuel2580@gmail.com>
2022-08-31 13:57:11 +0000

    added 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    if (key in a_dictionary):
-        a_dictionary[key] = value
-    else:
-     a_dictionary[key] = value
+def update_dictionary(my_dict, key, value):
+    if isinstance(key, str):
+        my_dict[key] = value
+    return 
