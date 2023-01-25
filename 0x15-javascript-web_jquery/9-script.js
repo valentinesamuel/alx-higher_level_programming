b264236094ca5c12956adaf609600982ea2608f5
valentinesamuel <valentinesamuel2580@gmail.com>
2023-01-25 21:00:22 +0100

    FEAT: Added js files

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,10 @@
+const getDivHello = $('DIV#hello');
+const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
+
+$(function () {
+    $.getJSON(url, function (data) {
+        const getHello = JSON.stringify(data).hello;
+        const addTag = $('<p></p>').text(getHello);
+        getDivHello.append(addTag);
+    });
+});
